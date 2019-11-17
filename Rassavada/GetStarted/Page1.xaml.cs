ce3115a8b2a4a8fcd4698e7fd3baaee90ba2406e
Aswin P G <aswinpg@outlook.in>
2019-11-17 14:38:39 +0530

    Full Wrong

--- Rassavada/GetStarted/Page1.xaml.cs
+++ Rassavada/GetStarted/Page1.xaml.cs
@@ -1,6 +1,6 @@
 ﻿
 
-using Rassavada.VerificationPage;
+//using Rassavada.VerificationPage;
 using System;
 
 using System.Collections.Generic;
@@ -20,44 +20,44 @@ namespace Rassavada
 
         }
 
-        private async void Back(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage());
-        }
-        public Color BarBackgroundColor { get; internal set; }
+        //private async void Back(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushModalAsync(new NavigationPage());
+        //}
+        //public Color BarBackgroundColor { get; internal set; }
 
-        private async void Submit(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new Xamarin.Forms.NavigationPage(new SubmitClicked()));
-        }
+        //private async void Submit(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushModalAsync(new Xamarin.Forms.NavigationPage(new SubmitClicked()));
+        //}
 
-        private void Choose(object sender, EventArgs e)
-        {
-            if (Choice.IsVisible == true)
-            {
-                Choice.IsVisible = false;
+        //private void Choose(object sender, EventArgs e)
+        //{
+        //    if (Choice.IsVisible == true)
+        //    {
+        //        Choice.IsVisible = false;
 
-            }
-            else
-            {
-                Choice.IsVisible = true;
-            }
-        }
+        //    }
+        //    else
+        //    {
+        //        Choice.IsVisible = true;
+        //    }
+        //}
 
-        private void HaveVehicle(object sender, EventArgs e)
-        {
-            Option.Text = "Yes";
-            Option.TextColor = Color.FromHex("#131313");
+        //private void HaveVehicle(object sender, EventArgs e)
+        //{
+        //    Option.Text = "Yes";
+        //    Option.TextColor = Color.FromHex("#131313");
 
-        }
+        //}
 
-        private void NoVehicle(object sender, EventArgs e)
-        {
+        //private void NoVehicle(object sender, EventArgs e)
+        //{
 
-            Option.Text = "No";
-            Option.TextColor = Color.FromHex("#131313");
+        //    Option.Text = "No";
+        //    Option.TextColor = Color.FromHex("#131313");
 
-        }
+        //}
     }
 
 
